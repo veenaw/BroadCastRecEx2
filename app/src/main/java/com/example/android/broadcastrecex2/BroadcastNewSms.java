b38e625e4d5f46a954ab1367246d5d8ae7d6546d
veenaw <vwagle2@gmail.com>
2016-09-18 00:32:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/broadcastrecex2/BroadcastNewSms.java
@@ -0,0 +1,13 @@
+package com.example.android.broadcastrecex2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BroadcastNewSms extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast_new_sms);
+    }
+}
